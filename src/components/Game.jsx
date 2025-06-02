8358b30298beadfa85d3f7c103abb1aa29b7c497
Alessandro <aleluinisilva@gmail.com>
2025-06-02 19:04:34 -0300

    adição de algumas funcionalidades como, centralização da camera no mario e mudanças na movimentação

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -12,15 +12,34 @@ export const Game = () => {
       }
       preload() {
         this.load.image("player", "/Mario.webp");
+        this.load.image("chao", "/chao.webp")
       }
+
+      geraChao(){
+        this.chaoGroup = this.physics.add.staticGroup();
+
+        for(let i = 0; i < 20; i++){
+          this.chao = this.add.sprite(0, 400, "chao");
+          this.chao.setOrigin(-i, -1.68);
+          this.chao.setScale(0.17);
+        }
+      }
+
       create() {
         this.player = this.add.sprite(0, 400, "player");
         this.player.setOrigin(0, 0);
         this.player.setScale(0.17);
+        this.geraChao()
         this.cursors = this.input.keyboard.createCursorKeys();
+
+        //segue o mario ai 
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.setBounds(0, 0, 1600, 600);
       }
       update() {
-        if (this.cursors.right.isDown) {
+        if(this.cursors.right.isDown && this.cursors.left.isDown){
+           this.player.x += 0;
+        }else if (this.cursors.right.isDown) {
           this.player.x += 6;
           this.player.flipX = false; 
         } else if (this.cursors.left.isDown) {
