4ca37191d73b66130d99049c6b784fa84b8e8c59
João Carlos Formento <jcformento13@gmail.com>
2025-06-03 00:24:17 -0300

    Sprite, animação, pulo e um monte de coisa

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import Phaser from "phaser";
 
 export const Game = () => {
@@ -11,40 +11,108 @@ export const Game = () => {
         super("MainScene");
       }
       preload() {
-        this.load.image("player", "/Mario.webp");
-        this.load.image("chao", "/chao.webp")
+        this.load.image("chao", "/Chao.png");
+        this.load.spritesheet("teste", "/MarioMiniSpritesheet.png", {
+          frameWidth: 16,
+          frameHeight: 22,
+        });
       }
 
-      geraChao(){
+      geraChao() {
         this.chaoGroup = this.physics.add.staticGroup();
 
-        for(let i = 0; i < 20; i++){
-          this.chao = this.add.sprite(0, 400, "chao");
-          this.chao.setOrigin(-i, -1.68);
-          this.chao.setScale(0.17);
+        for (let i = 0; i < 10; i++) {
+          const chao = this.chaoGroup.create(i * 300, 500, "chao");
+          chao.setOrigin(0, 0);
+          chao.setScale(0.75);
+          chao.refreshBody();
         }
       }
 
       create() {
-        this.player = this.add.sprite(0, 400, "player");
+        this.player = this.physics.add.sprite(0, 300, "teste", 2);
+        this.player.setCollideWorldBounds(true);
+        this.player.setBounce(0);
         this.player.setOrigin(0, 0);
-        this.player.setScale(0.17);
-        this.geraChao()
+        this.player.setScale(3);
+        this.geraChao();
+        this.physics.world.setBounds(0, 0, 2000, 600);
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.physics.add.collider(this.player, this.chaoGroup);
 
-        //segue o mario ai 
+        //segue o mario ai
         this.cameras.main.startFollow(this.player);
-        this.cameras.main.setBounds(0, 0, 1600, 600);
+        this.cameras.main.setBounds(0, 0, 2000, 600);
+
+        //Animação Mini Mario
+        this.anims.create({
+          key: "andando",
+          frames: [
+            { key: "teste", frame: 3 },
+            { key: "teste", frame: 2 },
+          ],
+          frameRate: 12,
+          repeat: -1,
+        });
+        this.anims.create({
+          key: "parado",
+          frames: [{ key: "teste", frame: 2 }],
+        });
+        this.anims.create({
+          key: "pulando",
+          frames: [{ key: "teste", frame: 1 }],
+          frameRate: 1,
+        });
+        this.anims.create({
+          key: "caindo",
+          frames: [{ key: "teste", frame: 0 }],
+          frameRate: 1,
+        });
       }
       update() {
-        if(this.cursors.right.isDown && this.cursors.left.isDown){
-           this.player.x += 0;
-        }else if (this.cursors.right.isDown) {
-          this.player.x += 6;
-          this.player.flipX = false; 
+        
+        //Movimentação eixo X
+
+        if (this.cursors.right.isDown) {
+          this.player.x += 10;
+          this.player.flipX = false;
+          //esses Ifs sao para as animações não ficarem repetindo infinitamente
+          if (this.player.anims.currentAnim?.key !== "andando") {
+            this.player.anims.play("andando");
+          }
         } else if (this.cursors.left.isDown) {
-          this.player.x -= 6;
-          this.player.flipX = true; 
+          this.player.x -= 10;
+          this.player.flipX = true;
+          if (this.player.anims.currentAnim?.key !== "andando") {
+            this.player.anims.play("andando");
+          }
+        } else if (this.cursors.left.isDown && this.cursors.right.isDown) {
+          this.player.x += 0;
+          //e esses para nao ficar repetindo animação de ficar parado atoa quando ja esta parado
+          if (this.player.anims.currentAnim?.key !== "parado") {
+            this.player.anims.play("parado");
+          }
+        } else {
+          if (this.player.anims.currentAnim?.key !== "parado") {
+            this.player.anims.play("parado");
+          }
+        }
+
+        //Movimentação eixo Y
+
+        if (this.player.body.velocity.y < 0) {
+          if (this.player.anims.currentAnim?.key !== "pulando") {
+            this.player.anims.play("pulando");
+          }
+        }
+        if (this.player.body.velocity.y > 0) {
+          if (this.player.anims.currentAnim?.key !== "caindo") {
+            this.player.anims.play("caindo");
+          }
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+          this.player.setVelocityY(-1500);
         }
       }
     }
@@ -57,7 +125,8 @@ export const Game = () => {
         physics: {
           default: "arcade",
           arcade: {
-            gravity: { y: 0 },
+            gravity: { y: 5000 },
+            debug: true,
           },
         },
         scene: MainScene,
