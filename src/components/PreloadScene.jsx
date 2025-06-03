660a51425de7229bb8a84b03371eb124ee412cab
Kauã Klein <kaua.klein@oi.net.br>
2025-06-03 20:57:08 -0300

    troca da cor de fundo

--- src/components/PreloadScene.jsx
+++ src/components/PreloadScene.jsx
@@ -6,7 +6,7 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
-    this.cameras.main.setBackgroundColor("#ffffff")
+    this.cameras.main.setBackgroundColor("#20b2aa")
     this.add.text(320, 280, "Carregando...", {
       fontFamily: "sans-serif",
       fontSize: "24px",
