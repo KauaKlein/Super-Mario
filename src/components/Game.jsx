cc07ffca97c9909cd4c4ce2de0c2458c4a9161d2
ALESSANDRO LUINI DA SILVA COUTINHO <alessandro_coutinho@estudante.sesisenai.org.br>
2025-06-03 16:00:55 -0300

    função game Over

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -4,14 +4,17 @@ import Phaser from "phaser";
 export const Game = () => {
   const gameRef = useRef(null);
   const phaserGameRef = useRef(null);
+  const GameOver = false
 
   useEffect(() => {
     class MainScene extends Phaser.Scene {
       constructor() {
         super("MainScene");
       }
+
       preload() {
         this.load.image("chao", "/Chao.png");
+        this.load.image("goomba", "/Goomba.png");
         this.load.spritesheet("teste", "/MarioMiniSpritesheet.png", {
           frameWidth: 16,
           frameHeight: 22,
@@ -21,30 +24,53 @@ export const Game = () => {
       geraChao() {
         this.chaoGroup = this.physics.add.staticGroup();
 
-        for (let i = 0; i < 10; i++) {
-          const chao = this.chaoGroup.create(i * 300, 500, "chao");
+        for (let i = 0; i < 2; i++) {
+          const chao = this.chaoGroup.create(i * 900, 500, "chao");
           chao.setOrigin(0, 0);
           chao.setScale(0.75);
           chao.refreshBody();
         }
       }
 
+      criagoomba() {
+        this.goombaGroup = this.physics.add.staticGroup();
+        const goomba = this.goombaGroup.create(500, 460, "goomba");
+        goomba.setOrigin(0, 0);
+        goomba.setScale(0.2);
+        goomba.refreshBody();
+      }
+
       create() {
+        this.colidiuComgoomba = false;
+
         this.player = this.physics.add.sprite(0, 300, "teste", 2);
         this.player.setCollideWorldBounds(true);
         this.player.setBounce(0);
         this.player.setOrigin(0, 0);
         this.player.setScale(3);
+
         this.geraChao();
+        this.criagoomba();
+
         this.physics.world.setBounds(0, 0, 2000, 600);
         this.cursors = this.input.keyboard.createCursorKeys();
+
         this.physics.add.collider(this.player, this.chaoGroup);
 
-        //segue o mario ai
+        // colisão com goomba
+        this.physics.add.collider(
+          this.player,
+          this.goombaGroup,
+          this.onPlayerHitObstacle,
+          null,
+          this
+        );
+
+        // câmera seguindo o Mario
         this.cameras.main.startFollow(this.player);
         this.cameras.main.setBounds(0, 0, 2000, 600);
 
-        //Animação Mini Mario
+        // animações
         this.anims.create({
           key: "andando",
           frames: [
@@ -54,29 +80,47 @@ export const Game = () => {
           frameRate: 12,
           repeat: -1,
         });
+
         this.anims.create({
           key: "parado",
           frames: [{ key: "teste", frame: 2 }],
         });
+
         this.anims.create({
           key: "pulando",
           frames: [{ key: "teste", frame: 1 }],
           frameRate: 1,
         });
+
         this.anims.create({
           key: "caindo",
           frames: [{ key: "teste", frame: 0 }],
           frameRate: 1,
         });
       }
-      update() {
-        
-        //Movimentação eixo X
 
+      onPlayerHitObstacle(player, goomba) {
+        if (this.colidiuComgoomba) return;
+
+        this.colidiuComgoomba = true;
+        console.log("Colisão detectada!");
+        player.setTint(0xff0000);
+        this.gameOver();
+      }
+
+      gameOver() {
+        this.cameras.main.fadeOut(1000);
+      }
+
+      // collectCoin(player, coin) {
+      //   coin.disableBody(true, true);
+      //   console.log("Moeda coletada!");
+      // }
+
+      update() {
         if (this.cursors.right.isDown) {
           this.player.x += 10;
           this.player.flipX = false;
-          //esses Ifs sao para as animações não ficarem repetindo infinitamente
           if (this.player.anims.currentAnim?.key !== "andando") {
             this.player.anims.play("andando");
           }
@@ -86,26 +130,17 @@ export const Game = () => {
           if (this.player.anims.currentAnim?.key !== "andando") {
             this.player.anims.play("andando");
           }
-        } else if (this.cursors.left.isDown && this.cursors.right.isDown) {
-          this.player.x += 0;
-          //e esses para nao ficar repetindo animação de ficar parado atoa quando ja esta parado
-          if (this.player.anims.currentAnim?.key !== "parado") {
-            this.player.anims.play("parado");
-          }
         } else {
           if (this.player.anims.currentAnim?.key !== "parado") {
             this.player.anims.play("parado");
           }
         }
 
-        //Movimentação eixo Y
-
         if (this.player.body.velocity.y < 0) {
           if (this.player.anims.currentAnim?.key !== "pulando") {
             this.player.anims.play("pulando");
           }
-        }
-        if (this.player.body.velocity.y > 0) {
+        } else if (this.player.body.velocity.y > 0) {
           if (this.player.anims.currentAnim?.key !== "caindo") {
             this.player.anims.play("caindo");
           }
@@ -116,6 +151,7 @@ export const Game = () => {
         }
       }
     }
+
     if (!phaserGameRef.current) {
       phaserGameRef.current = new Phaser.Game({
         type: Phaser.AUTO,
@@ -133,6 +169,7 @@ export const Game = () => {
         parent: gameRef.current,
       });
     }
+
     return () => {
       if (phaserGameRef.current) {
         phaserGameRef.current.destroy(true);
