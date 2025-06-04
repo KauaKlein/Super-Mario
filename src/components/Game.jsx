8dbf8113cd6f150746fa9938f620cd1a0967b000
João Carlos Formento <jcformento13@gmail.com>
2025-06-04 00:44:23 -0300

    Movimentação e spritesheet foram atualizados e foi adicionado movimentação de agachar

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -12,14 +12,17 @@ export const Game = () => {
       }
       preload() {
         this.load.image("chao", "/Chao.png");
-        this.load.image("MarioAgachado", "/MarioAgachado.png");
+        this.load.spritesheet("MarioAgachado", "/MarioAgachado.png", {
+          frameWidth: 16,
+          frameHeight: 16,
+        });
         this.load.spritesheet("MarioGameOver", "/MarioGameOver.png", {
           frameWidth: 16,
           frameHeight: 24,
         });
         this.load.spritesheet(
           "MiniMarioSpriteSheet",
-          "/MarioMiniSpritesheet.png",
+          "/MiniMarioSpritesheet.png",
           {
             frameWidth: 16,
             frameHeight: 22,
@@ -47,9 +50,10 @@ export const Game = () => {
         );
         this.player.setCollideWorldBounds(true);
         this.player.setBounce(0);
-        this.player.setOrigin(0, 0);
+        this.player.setOrigin(0, 1);
         this.player.setScale(3);
         this.player.isAgachado = false;
+        this.player.wasAgachado = false;
         this.player.isOlhandoFrente = true;
         this.geraChao();
         this.physics.world.setBounds(0, 0, 2000, 600);
@@ -62,7 +66,7 @@ export const Game = () => {
 
         //Animação Mini Mario
         this.anims.create({
-          key: "andando",
+          key: "andandoFrente",
           frames: [
             { key: "MiniMarioSpriteSheet", frame: 3 },
             { key: "MiniMarioSpriteSheet", frame: 2 },
@@ -71,17 +75,50 @@ export const Game = () => {
           repeat: -1,
         });
         this.anims.create({
-          key: "parado",
+          key: "paradoFrente",
           frames: [{ key: "MiniMarioSpriteSheet", frame: 2 }],
         });
         this.anims.create({
-          key: "pulando",
+          key: "pulandoFrente",
           frames: [{ key: "MiniMarioSpriteSheet", frame: 1 }],
           frameRate: 1,
         });
         this.anims.create({
-          key: "caindo",
+          key: "caindoFrente",
           frames: [{ key: "MiniMarioSpriteSheet", frame: 0 }],
+          frameRate: 1,
+        });
+        this.anims.create({
+          key: "andandoCosta",
+          frames: [
+            { key: "MiniMarioSpriteSheet", frame: 4 },
+            { key: "MiniMarioSpriteSheet", frame: 5 },
+          ],
+          frameRate: 12,
+          repeat: -1,
+        });
+        this.anims.create({
+          key: "paradoCosta",
+          frames: [{ key: "MiniMarioSpriteSheet", frame: 5 }],
+        });
+        this.anims.create({
+          key: "pulandoCosta",
+          frames: [{ key: "MiniMarioSpriteSheet", frame: 6 }],
+          frameRate: 1,
+        });
+        this.anims.create({
+          key: "caindoCosta",
+          frames: [{ key: "MiniMarioSpriteSheet", frame: 7 }],
+          frameRate: 1,
+        });
+        this.anims.create({
+          key: "olhandoFrente",
+          frames: [{ key: "MarioAgachado", frame: 0 }],
+          frameRate: 1,
+        });
+        this.anims.create({
+          key: "olhandoCosta",
+          frames: [{ key: "MarioAgachado", frame: 1 }],
           frameRate: 1,
         });
         this.anims.create({
@@ -96,57 +133,93 @@ export const Game = () => {
         });
       }
       update() {
-        //Movimentação eixo X
-        if (this.cursors.left.isDown && this.cursors.right.isDown) {
-          this.player.x += 0;
-          //e esses para nao ficar repetindo animação de ficar parado atoa quando ja esta parado
-          if (this.player.anims.currentAnim?.key !== "parado") {
-            this.player.anims.play("parado");
-          }
-        } else if (this.cursors.down.isDown && !this.player.isAgachado) {
-          this.player.isAgachado = true;
-          this.player.setTexture("MarioAgachado");
-          if (this.cursors.left.isDown && this.player.isOlhandoFrente) {
-            this.player.flipX = true;
-          } else {
-            this.player.flipX = false;
-          }
-        } else if (!this.cursors.down.isDown && this.player.isAgachado) {
-          this.player.isAgachado = false;
-          this.player.anims.play("parado");
-        } else if (this.cursors.right.isDown && !this.player.isAgachado) {
-          this.player.x += 10;
-          this.player.flipX = false;
+
+        //Klein, NÃO mexe nisso, eu não vou saber fazer de novo, prioriza o meu  ao do Ale
+        this.player.isIndoEsquerda = this.cursors.left.isDown;
+        this.player.isIndoDireita = this.cursors.right.isDown;
+        this.player.isPulando =
+          this.cursors.up.isDown && this.player.body.touching.down;
+        this.player.isAgachando = this.cursors.down.isDown;
+        this.player.isSubindo = this.player.body.velocity.y < 0;
+        this.player.isDescendo = this.player.body.velocity.y > 0;
+        
+
+        if (this.player.isAgachando) {
+          this.player.wasAgachado = true;
+          this.player.body.setOffset(0, 5);
+          this.player.body.setSize(16, 12);
+        } else if (!this.player.isAgachando &&  this.player.wasAgachado) {
+          this.player.y -= 2;
+
+          this.player.wasAgachado = false;
+          this.player.body.setSize(16, 22);
+          this.player.body.setOffset(0, 0);
+        }
+
+        let velocidade = 0;
+
+        if (this.player.isIndoEsquerda && !this.player.isAgachando) {
+          velocidade = -700;
           this.player.isOlhandoFrente = false;
-          //esses Ifs sao para as animações não ficarem repetindo infinitamente
-          if (this.player.anims.currentAnim?.key !== "andando") {
-            this.player.anims.play("andando");
-          }
-        } else if (this.cursors.left.isDown && !this.player.isAgachado) {
-          this.player.x -= 10;
-          this.player.flipX = true;
+        } else if (this.player.isIndoDireita && !this.player.isAgachando) {
+          velocidade = 700;
           this.player.isOlhandoFrente = true;
-          if (this.player.anims.currentAnim?.key !== "andando") {
-            this.player.anims.play("andando");
+        }
+        this.player.setVelocityX(velocidade);
+
+        if (this.player.isPulando) {
+          this.player.setVelocityY(-1600);
+        }
+
+        if (this.player.isAgachando) {
+          if (this.player.isIndoDireita) {
+            this.player.isOlhandoFrente = true;
+          } else if (this.player.isIndoEsquerda) {
+            this.player.isOlhandoFrente = false;
+          }
+          const direcao = this.player.isOlhandoFrente
+            ? "olhandoFrente"
+            : "olhandoCosta";
+          if (this.player.anims.currentAnim?.key !== direcao) {
+            this.player.anims.play(direcao);
+          }
+          return;
+        }
+
+        if (this.player.isSubindo) {
+          const direcao = this.player.isOlhandoFrente
+            ? "pulandoFrente"
+            : "pulandoCosta";
+          if (this.player.anims.currentAnim?.key !== direcao) {
+            this.player.anims.play(direcao);
+          }
+          return;
+        }
+
+        if (this.player.isDescendo) {
+          const direcao = this.player.isOlhandoFrente
+            ? "caindoFrente"
+            : "caindoCosta";
+          if (this.player.anims.currentAnim?.key !== direcao) {
+            this.player.anims.play(direcao);
+          }
+          return;
+        }
+
+        if (velocidade !== 0) {
+          const anim = this.player.isOlhandoFrente
+            ? "andandoFrente"
+            : "andandoCosta";
+          if (this.player.anims.currentAnim?.key !== anim) {
+            this.player.anims.play(anim);
           }
         } else {
-          if (this.player.anims.currentAnim?.key !== "parado") {
-            this.player.anims.play("parado");
+          const anim = this.player.isOlhandoFrente
+            ? "paradoFrente"
+            : "paradoCosta";
+          if (this.player.anims.currentAnim?.key !== anim) {
+            this.player.anims.play(anim);
           }
-        }
-        //Movimentação eixo Y
-        if (this.player.body.velocity.y < 0) {
-          if (this.player.anims.currentAnim?.key !== "pulando") {
-            this.player.anims.play("pulando");
-          }
-        }
-        if (this.player.body.velocity.y > 0) {
-          if (this.player.anims.currentAnim?.key !== "caindo") {
-            this.player.anims.play("caindo");
-          }
-        }
-        if (this.cursors.up.isDown && this.player.body.touching.down) {
-          this.player.setVelocityY(-1500);
         }
       }
     }
